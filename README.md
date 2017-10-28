d2e4c728cf640305a5f44521ca5aad5875d8f280
shashank <keshavashashank@gmail.com>
2017-10-28 23:35:48 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,19 @@
+![Mindtree Logo](https://www.mindtree.com/sites/default/files/mindtree-welcome-to-possible-logo-new.svg)
+# Mindtree Interview Coding Assignment
+
+The following Mindtree Assignment was part of interview process. The assignment objectives where
+
+1. List all users in the order they signed up on GitHub. Using [Get all Users](https://developer.github.com/v3/users/#get-all-users) Document.
+2. Display Users Avatar and login name.
+3. On click of Avatar display a window on the right panel with his [Users Bio details](https://developer.github.com/v3/users/#get-a-single-user) must include id,name,company,location,public repos and followers.
+4. Display pagination.
+
+## Installation
+1. Clone or download the repo
+2. Register [OAuth App](https://developer.github.com/apps/building-integrations/setting-up-and-registering-oauth-apps/registering-oauth-apps/)
+3. Install Dependencies `yarn install or npm install`
+4. Start the app `yarn start or npm start`
+
+## License
+
+## Acknowledgment
---
+++ src/App.js
@@ -0,0 +1,117 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import request from "superagent";
+
+const clientID = "38481dd283afc97431c0";
+const clientSecret = "f8edbfdcd42e084b2dcf5b22cc6f42a8a6cc3205";
+
+const url = `https://api.github.com/users?client_id=${clientID}&client_secret=${clientSecret}`;
+
+class App extends Component {
+  constructor(props) {
+    super(props); // Super allows to use constructor methods i.e this.props of parent class
+    this.state = {
+      userList: null,
+      userDetails: null,
+      apiResponse: null
+    };
+  }
+
+  handleCloseBio = () => {
+    this.setState({
+      userDetails: null
+    });
+  };
+  handleClick = user => {
+    let userBio = null;
+
+    request.get(user.url).end((err, res) => {
+      if (err) console.log("Fetch User Details Failed", err);
+      else {
+        userBio = (
+          <div className={"basicDetails"}>
+            <div className={"basicDetails-header"}>
+              <h3>
+                {res.body.login} User Bio
+              </h3>
+              <a className={"basicDetails-close"} onClick={this.handleCloseBio}>
+                X
+              </a>
+            </div>
+            <ol>
+              <li className={"align-left"}>
+                Id:{res.body.id}
+              </li>
+              <li className={"align-left"}>
+                Name:{res.body.name}
+              </li>
+              <li className={"align-left"}>
+                Company:{res.body.company}
+              </li>
+              <li className={"align-left"}>
+                Location:{res.body.location}
+              </li>
+              <li className={"align-left"}>
+                Public Repos:{res.body.public_repos}
+              </li>
+              <li className={"align-left"}>
+                Followers:{res.body.followers}
+              </li>
+            </ol>
+          </div>
+        );
+        this.setState({
+          userDetails: userBio
+        });
+      }
+    });
+  };
+  _userListing = () => {
+    let users = [];
+    if (!!this.state.apiResponse) {
+      users = this.state.apiResponse.map(user => {
+        // Bind helps to bind this and multiple parameters to the event handler, arrow frunctions (=>) can also be used to bind
+        return (
+          <div
+            key={user.id}
+            className={"userTiles"}
+            onClick={this.handleClick.bind(this, user)}
+          >
+            <img className={"avatar"} src={user.avatar_url} />
+            <b className={"login"}>
+              {user.login}
+            </b>
+          </div>
+        );
+      });
+    }
+    return (
+      <div>
+        {users}
+      </div>
+    );
+  };
+  componentDidMount = () => {
+    request.get(url).end((err, res) => {
+      if (err) console.log(err);
+      else
+        this.setState({
+          apiResponse: res.body
+        });
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Mindtree Interview Coding Assignment</h1>
+        </header>
+        {this._userListing()}
+        {this.state.userDetails}
+      </div>
+    );
+  }
+}
+
+export default App;
